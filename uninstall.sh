d9e688adbbd55b9cbd106f772b6688ef03ceed3f
Morten Daniel Fornes <mdfornes@gmail.com>
2021-10-21 18:24:48 +0700

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [[ "$OSTYPE" != darwin* ]]; then
+    >&2 echo "This script needs to run on macOS"
+    exit 1
+fi
+
+rm -rf "/Applications/fuse X (uri-handler).app" \
+       "/Applications/fuse X.app" \
+       "/usr/local/lib/node_modules/@fuse-x" \
+       "/usr/local/bin/fuse-x" \
+       "/usr/local/bin/fuse" \
+       "/usr/local/bin/uno"
+
+echo "fuse X has been uninstalled"
